0570886bd7d7c49f4b1a24e29e4f6d270df05c52
guy <asulpizio62@haverfordsd.net>
2020-10-15 10:30:43 -0400

    MVPMVP

--- GesturesDemo/ViewController.swift
+++ GesturesDemo/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func imageviewTapped(_ sender: UITapGestureRecognizer) {
+        print("Shara Ishvalda")
+    }
+    
+    @IBAction func dragonPanned(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: view)
 
+        // 2
+        guard let gestureView = sender.view else {
+          return
+        }
 
+        gestureView.center = CGPoint(
+          x: gestureView.center.x + translation.x,
+          y: gestureView.center.y + translation.y
+        )
+
+        // 3
+        sender.setTranslation(.zero, in: view)
+    }
 }
 
